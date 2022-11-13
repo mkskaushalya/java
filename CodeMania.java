be44cafccd474704116c09c7e0e75f5718067130
Sahan Kaushalya <mkskaushalya@gmail.com>
2022-11-13 17:55:02 +0530

    Create CodeMania.java

---
+++ CodeMania.java
@@ -0,0 +1,19 @@
+public class CodeMania {
+    public static void details() {
+      double version = 3.0;
+      int teamMembers = 3;
+      System.out.println("Register Now");
+      String registrationClosing = "12th October Midnight";
+      //Via HackerRank
+      boolean registration = true;
+      if(registration == true){
+        String  eventDate = "20-10-2022";
+        String  eventStartTime = "18:00";
+        float myFloatNum = 5.99f;
+        System.out.println("12 Hours Long");
+        System.out.println("Algorithmic Programming Challenge");
+        System.out.println("With valuable cash Prices, Giftpack & Certificate");
+      }
+
+    }
+  }
